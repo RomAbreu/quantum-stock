cc58e9224a8c96ff7b0c1de972631c0fa502154e
Steven <smmr0001@ce.pucmm.edu.do>
2025-07-03 01:05:47 -0400

    feat: Add product categories, configuration constants, and routes; remove obsolete constants

---
+++ frontend/quantum-stock/src/lib/constants/config.constants.ts
@@ -0,0 +1,15 @@
+export const NEXT_API_URL =
+	process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080/api/v1';
+
+export const NEXT_KEYCLOAK_URL =
+	process.env.NEXT_KEYCLOAK_URL ?? 'http://localhost:9090';
+
+export const NEXT_KEYCLOAK_AUTH_URL: string =
+	process.env.NEXT_KEYCLOAK_AUTH_URL ??
+	'http://localhost:9090/realms/quantum-stock/account';
+
+export const NEXT_KEYCLOAK_REALM =
+	process.env.NEXT_KEYCLOAK_REALM ?? 'quantum-stock';
+
+export const NEXT_KEYCLOAK_CLIENT_ID =
+	process.env.NEXT_KEYCLOAK_CLIENT_ID ?? 'quantum-stock-frontend';
--- frontend/quantum-stock/src/lib/constants/endpoints.constants.ts
+++
@@ -1,4 +0,0 @@
-export enum EndpointEnum {
-	Login = '/api/v1/auth/login',
-	Register = '/api/v1/auth/register',
-}
