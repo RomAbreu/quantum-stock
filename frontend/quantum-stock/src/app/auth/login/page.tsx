ed859ff9fa9d228ed02053dafe13c3da6e374a7e
Steven <smmr0001@ce.pucmm.edu.do>
2025-07-03 01:04:46 -0400

    feat: Remove authentication components and actions to streamline the codebase

--- frontend/quantum-stock/src/app/auth/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import LoginUI from '@/components/pages/login/LoginUI';
-
-export default function LogInPage() {
-	return <LoginUI />;
-}
--- frontend/quantum-stock/src/app/auth/signup/page.tsx
+++
@@ -1,5 +0,0 @@
-import RegisterUI from '@/components/pages/register/RegisterUI';
-
-export default function SignUpPage() {
-	return <RegisterUI />;
-}
