05d5e6fcba7d45ec7911b444c87f7a40fb666abe
Romario Abreu <rias0331@gmail.com>
2025-08-21 21:02:30 -0400

    feat: paginating get all inventory movements endpoint

--- backend/src/main/java/org/example/backend/controllers/InventoryMovementController.java
+++ backend/src/main/java/org/example/backend/controllers/InventoryMovementController.java
@@ -3,8 +3,11 @@ package org.example.backend.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.example.backend.dtos.InventoryMovementResponse;
+import org.example.backend.dtos.PaginatedResponse;
 import org.example.backend.models.InventoryMovement;
 import org.example.backend.services.InventoryMovementService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +23,13 @@ public class InventoryMovementController {
     private final ObjectMapper objectMapper;
 
     @GetMapping
-    public ResponseEntity<List<InventoryMovementResponse>> getAllInventoryMovements() {
-        List<InventoryMovement> inventoryMovements = inventoryMovementService.getAllInventoryMovements();
+    public ResponseEntity<PaginatedResponse<InventoryMovementResponse>> getAllInventoryMovements(Pageable pageable) {
+        Page<InventoryMovement> inventoryMovements = inventoryMovementService.getAllInventoryMovements(pageable);
 
         List<InventoryMovementResponse> inventoryMovementResponses = inventoryMovements.stream()
                 .map(movement -> objectMapper.convertValue(movement, InventoryMovementResponse.class))
                 .toList();
 
-        return ResponseEntity.ok(inventoryMovementResponses);
+        return ResponseEntity.ok(new PaginatedResponse<>(inventoryMovementResponses, inventoryMovements));
     }
 }
