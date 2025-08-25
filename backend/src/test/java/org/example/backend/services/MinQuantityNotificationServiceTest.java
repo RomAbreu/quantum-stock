c22c335dd6e3ab074149cb151fa068df60e89043
Romario Abreu <rias0331@gmail.com>
2025-08-24 22:08:00 -0400

    fix: tests can now be run from command line

--- backend/src/test/java/org/example/backend/services/MinQuantityNotificationServiceTest.java
+++ backend/src/test/java/org/example/backend/services/MinQuantityNotificationServiceTest.java
@@ -22,7 +22,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class MinQuantityNotificationServiceTest {
+public class MinQuantityNotificationServiceTest {
 
     @Mock
     private MinQuantityNotificationRepository minQuantityNotificationRepository;
